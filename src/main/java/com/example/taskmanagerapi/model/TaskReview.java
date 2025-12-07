3b289b6104e3a35b091f7d43ebfa0b3a3770ae38
Abduqodir <23abdukodir@gmail.com>
2025-12-08 01:08:07 +0300

    new class created

---
+++ src/main/java/com/example/taskmanagerapi/model/TaskReview.java
@@ -0,0 +1,17 @@
+package com.example.taskmanagerapi.model;
+
+public class TaskReview {
+
+    public void rename (){
+        String color = "red";
+
+        if(color.equalsIgnoreCase("red")){
+            System.out.println("good bye");
+        }
+        else{
+            System.out.println("bad bye");
+        }
+    }
+
+
+}
